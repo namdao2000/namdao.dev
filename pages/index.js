bcd5306a52cfa051896f4d048db1a9082468fd30
Nam Dao <nam.dao@eucalyptus.vc>
2022-05-12 21:42:57 +0700

    fix: few eslint errors

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,7 @@ import Paragraph from '../components/paragraph';
 import { BioSection, BioYear } from '../components/bio';
 import Layout from '../components/layouts/article';
 import Section from '../components/section';
-import {
-  IoLogoTwitter,
-  IoLogoInstagram,
-  IoLogoGithub,
-  IoLogoLinkedin,
-} from 'react-icons/io5';
+import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
 import Image from 'next/image';
 
 const ProfileImage = chakra(Image, {
