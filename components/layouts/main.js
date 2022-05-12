7ae1786677af0ae9a4af6037c810082a421ad655
Nam Dao <nam.dao@eucalyptus.vc>
2022-05-12 22:04:02 +0700

    feat: updated the dns

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -23,11 +23,11 @@ const Main = ({ children, router }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@craftzdog" />
         <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:image" content="https://www.namdao.dev/card.png" />
         <meta property="og:site_name" content="Nam Dao" />
         <meta name="og:title" content="Nam Dao" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
+        <meta property="og:image" content="https://www.namdao.dev/card.png" />
         <title>Nam Dao - Homepage</title>
       </Head>
 
