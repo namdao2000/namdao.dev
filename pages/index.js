54d52bc022c9ccb0dec5bed3337fa39e05ebe8b3
Nam Dao <nam.dao@eucalyptus.vc>
2022-05-12 15:53:03 +0700

    feat: added missing dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,227 @@
-import {Container, Box, Heading} from "@chakra-ui/react";
+import NextLink from 'next/link';
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+  chakra,
+} from '@chakra-ui/react';
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import Paragraph from '../components/paragraph';
+import { BioSection, BioYear } from '../components/bio';
+import Layout from '../components/layouts/article';
+import Section from '../components/section';
+import { GridItem } from '../components/grid-item';
+import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub } from 'react-icons/io5';
+import Image from 'next/image';
 
-const Page = () => {
-  return (
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: (prop) => ['width', 'height', 'src', 'alt'].includes(prop),
+});
+
+const Home = () => (
+  <Layout>
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
         Hello, I&apos;m a full-stack software engineer from Sydney!
       </Box>
 
-      <Box display={{md: 'flex'}}>
+      <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Nam Dao
           </Heading>
-          <p>Software Engineer (Loves to socialise - love a good drink - passionate about startups)</p>
+          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/takuya.jpg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
         </Box>
       </Box>
-    </Container>
-  )
-}
 
-export default Page
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Oh hi there, I see you&apos;re reading my bio ... I guess I&apos;ll
+          have to keep it interesting 😉 The &apos;boring&apos; stuff first: 📌
+          Full-Stack Nodejs Developer 📌 AWS Certified Cloud Practitioner &
+          Solutions Architect 📌 USYD - B. Advanced Computing 📌 Founded an
+          off-the-book gaming startup in the cloud when I was 15. I grew it to
+          3400 customers and raised $50k by 2020. Who am I? I am a tech-savvy
+          software engineer with a flair of leadership. At heart, I have a
+          passion for building things - web apps, APIs, automated CI pipeline
+          ... if it&apos;s tech, you name it! And I&apos;ll deliver. With a
+          plethora of experience in the full-stack development space, I am
+          always down to build cool new things 🚀
+          <NextLink href="/works/inkdrop" passHref scroll={false}>
+            <Link>Inkdrop</Link>
+          </NextLink>
+          . He publishes content for marketing his products and his YouTube
+          channel called &quot;
+          <NextLink href="https://www.youtube.com/devaslife" passHref>
+            <Link target="_blank">Dev as Life</Link>
+          </NextLink>
+          &quot; has more than 100k subscribers.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as="h3" variant="section-title">
+          Bio
+        </Heading>
+        <BioSection>
+          <BioYear>1984</BioYear>
+          Born in Osaka (大阪), Japan.
+        </BioSection>
+        <BioSection>
+          <BioYear>2010</BioYear>
+          Completed the Master&apos;s Program in the Graduate School of
+          Information Science at Nara Institute of Science and Technology
+          (奈良先端科学技術大学院大学情報科学研究科修士課程)
+        </BioSection>
+        <BioSection>
+          <BioYear>2010</BioYear>
+          Worked at Yahoo! Japan (ヤフー株式会社入社)
+        </BioSection>
+        <BioSection>
+          <BioYear>2012 to present</BioYear>
+          Works as a freelance
+        </BioSection>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          I ♥
+        </Heading>
+        <Paragraph>
+          Art, Music,{' '}
+          <Link href="https://illust.odoruinu.net/" target="_blank">
+            Drawing
+          </Link>
+          , Playing Drums,{' '}
+          <Link href="https://500px.com/p/craftzdog" target="_blank">
+            Photography
+          </Link>
+          , Leica, Machine Learning
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.3}>
+        <Heading as="h3" variant="section-title">
+          On the web
+        </Heading>
+        <List>
+          <ListItem>
+            <Link href="https://github.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoGithub />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/inkdrop_app" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @inkdrop_app (English)
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://twitter.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoTwitter />}
+              >
+                @craftzdog (日本語)
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://instagram.com/craftzdog" target="_blank">
+              <Button
+                variant="ghost"
+                colorScheme="teal"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @craftzdog
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            href="https://www.youtube.com/devaslife"
+            title="Dev as Life"
+          >
+            My YouTube channel (&gt;100k subs)
+          </GridItem>
+          <GridItem href="https://www.inkdrop.app/" title="Inkdrop">
+            A Markdown note-taking app
+          </GridItem>
+        </SimpleGrid>
+
+        <Box align="center" my={4}>
+          <NextLink href="/posts" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              Popular posts
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+    </Container>
+  </Layout>
+);
+
+export default Home;
+export { getServerSideProps } from '../components/chakra';
